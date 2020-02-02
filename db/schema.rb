4008ac0c7dd5e7b6d8d4d2506abbeba03f401695
rnasakazu <zer0.26053608@gmail.com>
2020-02-02 14:15:17 +0900

    Add Room model (#3)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_080107) do
+ActiveRecord::Schema.define(version: 2020_02_02_051125) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2020_02_01_080107) do
 
   create_table "customers", force: :cascade do |t|
     t.string "remember_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

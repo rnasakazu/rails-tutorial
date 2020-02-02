72f647da8da408672144ef40eba854f8560c5113
rnasakazu <zer0.26053608@gmail.com>
2020-02-03 00:33:07 +0900

    Add Room controller (#3)

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,16 @@
+class RoomsController < ApplicationController
+  before_action :registered_confirmation, only: %i[index show]
+  before_action :registered_confirmation, only: :widget
+
+  def index
+    @rooms = current_admin.rooms
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def widget
+    @room = current_customer.room
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :rooms, only: %i[index show]
+  get '/widget/floating_chat', to: 'rooms#widget', as: :floating_chat
 end

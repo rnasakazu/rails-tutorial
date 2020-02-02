185d2e5ef3b1b6f9f909dd216462ce3a9691bf93
rnasakazu <zer0.26053608@gmail.com>
2020-02-02 22:28:21 +0900

    Customize rspec(#3)

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,14 @@ module RailsTutorial
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
-      g.test_framework :rspec, 
-            view_specs: false, 
-            helper_specs: false, 
-            controller_specs: false, 
-            routing_specs: false,
-            request_spec: false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
